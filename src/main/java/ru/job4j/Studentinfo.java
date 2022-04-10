768831b0eb91e1d68c370ede03daf6009a0d1388
jin6655 <vasilypoiser@yandex.ru>
2022-04-10 17:36:19 +0300

    4.1. Разбор первой программы

---
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class Studentinfo {
+    public static void main(String[] args) {
+        System.out.println("Vasily Gromov");
+        System.out.println("28.05.1987");
+    }
+}
