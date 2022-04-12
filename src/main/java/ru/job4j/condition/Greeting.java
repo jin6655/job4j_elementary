1bd368f89617a711e9a4105397888ce58b3593c5
jin6655 <vasilypoiser@yandex.ru>
2022-04-12 22:11:34 +0300

    9.1. String

---
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -0,0 +1,14 @@
+package ru.job4j.condition;
+
+public class Greeting {
+
+    public static void main(String[] args) {
+        String id = "I like Java ";
+        System.out.println(id);
+        String ide = id + "But I am a newbie";
+        int year = 2022;
+        String idea = ide + " " + year;
+        System.out.println(idea);
+    }
+
+}
