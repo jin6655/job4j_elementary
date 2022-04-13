eb22287a6d0cfcf4bd810a93fea0cb721c47deca
jin6655 <vasilypoiser@yandex.ru>
2022-04-13 22:09:33 +0300

    0. Что такое тестировани

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -13,10 +13,17 @@ public class Converter {
     }
 
     public static void main(String[] args) {
-        float euro = Converter.rubleToEuro(140);
-        float dollar = Converter.rubleToDollar(5000);
-        System.out.println("140 rubles are " + euro + " euro");
-        System.out.println("5000 rubles are " + dollar + " dollar");
+        float ineuro = 140;
+        float indollar = 180;
+        float excepted = 2;
+        float exceptedd = 3;
+        float out = Converter.rubleToEuro(ineuro);
+        float outd = Converter.rubleToDollar(indollar);
+        boolean passed =  excepted == out;
+        boolean passedd = exceptedd == outd;
+        System.out.println("140 rubles are 2. Test result : " + passed);
+        System.out.println("180 rubles are 3. Test result for dollar : " + passedd);
     }
 
 }
+
